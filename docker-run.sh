ccf4fff31e7cfb9383b101305553c71b1125eba8
krsuhjunho <krsuhjunho@gmail.com>
2021-03-20 00:47:27 +0000

    2021-03-20 Delete docker-build.sh

--- docker-run.sh
+++ docker-run.sh
@@ -9,27 +9,86 @@ SERVER_IP=$(curl -s ifconfig.me)
 PC_URL="cgi-bin/dneo/dneo.cgi?"
 MOBILE_URL="cgi-bin/dneosp/dneosp.cgi"
 HTTP_BASE="http://"
+TIME_ZONE="Asia/Tokyo"
+TODAY=$(date "+%Y-%m-%d")
+COMMIT_COMMENT="$2"
+BUILD_OPTION="$1"
 
+
+DOCKER_IMAGE_BUILD()
+{
 docker build -t ${BASE_IMAGE_NAME} .
+}
 
+DOCKER_IMAGE_PUSH()
+{
+docker push ${BASE_IMAGE_NAME}
+}
+
+GIT_COMMIT_PUSH()
+{
+git add -u
+git commit -a -m "${TODAY} ${COMMIT_COMMENT}"
+git config credential.helper store
+git push origin main
+}
+
+DOCKER_CONTAINER_REMOVE()
+{
 docker rm -f ${DOCKER_CONTAINER_NAME}
+}
 
+DOCKER_CONTAINER_CREATE()
+{
 docker run -tid --privileged=true \
 -h "${CONTAINER_HOST_NAME}" \
 --name="${DOCKER_CONTAINER_NAME}" \
 -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
 -v /etc/localtime:/etc/localtime:ro \
--e TZ=Asia/Tokyo \
+-e TZ=${TIME_ZONE} \
 -p ${SSH_PORT}:22 -p ${HTTP_PORT}:80 \
 ${BASE_IMAGE_NAME}
 
+}
 
-#docker exec -it ${DOCKER_CONTAINER_NAME} /bin/bash
+DOCKER_CONTAINER_BASH()
+{
+docker exec -it ${DOCKER_CONTAINER_NAME} /bin/bash
+}
 
-
+DOCKER_CONTAINER_URL_SHOW()
+{
 echo ""
 echo "PC      URL => ${HTTP_BASE}${SERVER_IP}:${HTTP_PORT}/${PC_URL}"
 echo ""
 echo "MOBILE  URL => ${HTTP_BASE}${SERVER_IP}:${HTTP_PORT}/${MOBILE_URL}"
 echo ""
+
+}
+
+MAIN()
+{
+
+if [ "$BUILD_OPTION" == "--build" ]; then
+    DOCKER_IMAGE_BUILD
+	DOCKER_IMAGE_PUSH
+	GIT_COMMIT_PUSH
+fi
+
+DOCKER_CONTAINER_REMOVE
+DOCKER_CONTAINER_CREATE
+#DOCKER_CONTAINER_BASH
+DOCKER_CONTAINER_URL_SHOW
+}
+
+MAIN
+
+
+
+
+
+
+
+
+
 
